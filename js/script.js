cf7a316de83eed73f1a1340d8cecca46f1a210ea
ASUS Windows <sehdeva@proton.me>
2026-08-30 00:58:43 +0530

    onsubmit started

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 var elems = document.querySelectorAll('select');
 var instances = M.FormSelect.init(elems, {});
+
+
+function onSubmit() {
+	const formSociety = document.getElementById('form_society').value;
+	const formFlat = document.getElementById('form_flat').value;
+	const formContact = document.getElementById('form_phone').value;
+	const formIsSubOrder = document.getElementById('form_sub').checked;
+
+	const orderMessage = '';
+	const messageForWhatsapp = `Hello\nI'm from ${formFlat},${formSociety}.\n${orderMessage}\nMy contact is ${formContact}.`;
+	alert(messageForWhatsapp);
+}
